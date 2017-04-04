2d511f2e480c8c7ff24ace9ee99e36453df23a25
DDGammulle <damithagammulla@gmail.com>
2017-04-04 22:31:19 +0530

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+   
+class InstantiateHelloWorld
+{
+     public static void Main()
+     {
+          Form form   = new Form();
+          form.Text   = "Instantiate " + form.Text;
+          form.Paint += new PaintEventHandler(MyPaintHandler);
+   
+          Application.Run(form);
+     }
+     static void MyPaintHandler(object objSender, PaintEventArgs pea)
+     {
+          Form     form = (Form)objSender;
+          Graphics graphics = pea.Graphics;
+   
+          graphics.DrawString("Hello from InstantiateHelloWorld!", 
+                          form.Font, Brushes.Black, 0, 100);
+     }
+}
